b1134e63733449b741cc1ff400c3a31a43e7efab
Lisa Nam <lsyn2016@gmail.com>
2023-05-30 17:14:39 -0400

    completed day 1

--- day 1/if_else.py
+++ day 1/if_else.py
@@ -7,7 +7,21 @@ num = 4
 print("I don't know what num is but it's not 1 or 0")
 # and
 
-if True and False:
+# if True:
+#     print("It's true")
+
+# if False:
+#     print("It's false")
+
+
+num = 1
+print("Num is ", num)
+if num == 0:
+    print("It's 0!")
+if num == 1:
+    print("It's 1!")
+
+# &(and)
 
 # or
 if True or False:
