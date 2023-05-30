d838bc7bcf3ed381ef12f03e82cf91369864d6c1
Amanda_Ciriello <aciriello120643@me.bergen.edu>
2023-05-30 17:15:04 -0400

    Completed Day 1

--- day 1/helloWorld.py
+++ day 1/helloWorld.py
@@ -1,9 +1,16 @@
 # Output & Input: print, input
 
 # print Hello World!
+print ("HelloWorld!")
 
+<<<<<<< HEAD
 print ("HelloWorld!")
 
 # print Hello {user}! using input
 name = input("Amanda")
 print("Hello" + name)
+=======
+# print Hello {user}! using input
+name = input("Amanda")
+print("Hello" + name)
+>>>>>>> a373785 (Completed Day 1)
